46247b4a69c782d17050465777def74093bb81ef
roburrhoades <seth@roburhealth.com>
2019-11-01 20:27:44 -0400

    Add Age Source to json writeout. Update pass/fail checks and TODO

--- metadataExtract.py
+++ metadataExtract.py
@@ -6,22 +6,32 @@
 Expected values:
 GSM400641 - 4.28 - check
 GSM32928 - 7.5 - check
-GSM1402452 - n/a (however ages exist in the journal article but very hard to know which
+GSM1402452 - 15.14 (ages exist in the journal article but very hard to know which
                 ... age corresponds to the sample. However cell type is in description so 
-                    ... probably should be skipped anyways)
+                    ... probably should be skipped anyways) TODO
+
 GSM1418737 - 13.5 - check
 GSM1394796 - skipped - check (for dual channel)
-GSM172972 - n/a (however ages exist in the GSE series description, should be 8 weeks. Currently 
-                    two groups are sampled at different times (4-7 weeks and 8 weeks). Thus 
-                    13.5 is not correct, however WT is not explicityly mentioned either. Without
-                    access to article, should this be left blank or all groups just set at 13.5?)
+GSM172972 - 13.5 FAIL (Ages exist in the GSE series description, should be 8 weeks for
+                    this particular genotype. There is no duration flag, so these ages
+                    should not be summed. Two groups are sampled at different times 
+                    (4-7 weeks and 8 weeks). Thus 13.5 is not correct, however 
+                    WT is not explicitly mentioned either. Should this be left 
+                    blank or all groups just set at 8) TODO
+
 GSM1338039 - 9.42 - check
-GSM218336 - n/a - check (5hr is not valid, but treatment durations exist in the GSE description 
-                ... and sample names, and ages exist in the journal article.. should be 8 weeks)
+GSM218336 - 31.14 - FAIL (5hr should flag a possible duration of exposure, not age.
+                            4 time series divides the groups, all mice started exposure
+                            at 8 weeks. Somehow a trigger will need to be made to
+                            ID these durations, then, since no other age is found
+                            in the sample or study text, to hit the full text to
+                            extract the 8 weeks and add the exposures. Edge case!) TODO
+
 GSM1282831 - 16 - check
 GSM937915 - n/a - check (for cell == True)
-GSM557138 - 6 - check (however, induction began at 6 weeks and mice were only analyzed upon death, 
-                ... which ranged over months, this may be edge case where the true age cannot be extracted)
+GSM557138 - 6 - check (however, induction began at 6 weeks and mice were only 
+                    analyzed upon death, which ranged 20wks-1.5yrs. A check should
+                    be made on an extreme range of values) TODO
 """
 
 import re, requests, os, random, sys, json, random
@@ -34,7 +44,7 @@ refDirectory = 'refFiles'
 organism = 'Mus musculus'
 queryIDs = False
 queryNum = 10000
-overwrite = False
+overwrite = True
 
 if queryIDs == True:
     if organism == 'Mus musculus':
@@ -71,6 +81,7 @@ def main(sampleIDs, organism, metaDict):
             organism)), 'w') as fout:
             json.dump(metaDict, fout, indent = 4)
     
+    print('done')
 
 if __name__ == "__main__":
 

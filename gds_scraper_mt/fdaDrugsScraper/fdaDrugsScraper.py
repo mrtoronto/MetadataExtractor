3c8acf42cf71107472e191df7a86d2f940af5117
roburrhoades <seth@roburhealth.com>
2019-11-07 17:04:31 -0500

    Remove trailing whitespaces. Not all approvals are ANDAs: change key to 'entity' instead

--- gds_scraper_mt/fdaDrugsScraper/fdaDrugsScraper.py
+++ gds_scraper_mt/fdaDrugsScraper/fdaDrugsScraper.py
@@ -1,3 +1,9 @@
+import re, json, requests
+from lxml import html
+from string import ascii_lowercase
+whiteout = re.compile('^ +| +$')
+letters = [x for x in re.split('(\w)', ascii_lowercase) if x != '']
+
 def fdaDrugScraper(export=False):
     """
     Scrapes https://www.accessdata.fda.gov for drugs and associated data.
@@ -5,7 +11,7 @@ def fdaDrugScraper(export=False):
     drugs_dict = {}
     for letter in letters:
         url = f'https://www.accessdata.fda.gov/scripts/cder/daf/index.cfm?event=browseByLetter.page&productLetter={letter}'
-        root = lxml.html.document_fromstring(requests.get(url).text)
+        root = html.document_fromstring(requests.get(url).text)
         drug_list = root.findall('.//tbody/tr/td')
         for drug in drug_list:
             drug_cols = [re.sub('[\r\n\t]', '', i) for i in drug.itertext() if re.sub('[\r\n\t ]', '', i) != '']
@@ -16,12 +22,14 @@ def fdaDrugScraper(export=False):
             company = drug_cols[2].split('|')[3]
 
             drug_dict = {'drug_name' : drug_name,
-                        'ANDA' : anda_number,
-                        'active_ingred' : active_ingred,
-                        'dosage_form' : dosage_form,
-                        'company' : company
+                        'entity' : whiteout.sub('', anda_number),
+                        'active_ingred' : whiteout.sub('', active_ingred),
+                        'dosage_form' : whiteout.sub('', dosage_form),
+                        'company' : whiteout.sub('', company)
                         }
+
             drugs_dict[drug_name] = drug_dict
+
     if export == True:
         with open('fdaDrug_data.json', 'w') as fout:
             json.dump(drugs_dict, fout, indent = 4)

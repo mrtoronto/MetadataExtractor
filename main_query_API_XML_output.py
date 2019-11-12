ea4222d46b3402738eacf1bcf7778c2bde34844d
mrtoronto <34576341+mrtoronto@users.noreply.github.com>
2019-11-12 08:27:03 -0500

    Docstring updates

---
+++ main_query_API_XML_output.py
@@ -0,0 +1,78 @@
+import numpy as np
+import random
+import pandas as pd
+from src.scrape_gds import scrape_gds
+
+def main(sampleIDs, organism, out_path, multichannel_flag, cells_flag, metadata_filter, keep_files_list, out_types_list, run_type, local_files_list):
+    """
+    This function will take in a list of sampleIDs and create the specified output files based on the sample's data and associated meta-data.
+
+        Args:
+            `query_terms` - List: List of file names generated from the `data/GEO_MusmusculusSamples.csv` data file. Samples can be selected randomly or in order starting from a random spot.
+            `api_key` - Str: NCBI e-utils API key
+            `multichannel` - Boolean: Flag to filter out multichannel samples
+            `cells_flag` - Boolean: Flag to filter out samples marked as Cells (See `geoSampleCellCheck()`)
+            `metadata_filter` - Boolean: Flag to filter out samples that fail to join to any downloaded metadata.
+            `DEBUG` - Int: Number to determine the depth of debug print output (Deprecated).
+            `out_path` - Str: Filename of the output file. Excludes extensions as that'll be added depending on the type of the output.
+            `keep_files` - List: Intermediate files to keep. Options currently are 'xml' or 'txt'. Both may be selected at once.
+            `out_types` - List: File type of the output. Options currently include 'json' and 'csv'. Both may be selected at once.
+            `local_files_list` - List: List of types of files to check for a local version of before querying API and downloading. Currently only works with `txt` but will add `xml` in the future. 
+    """
+    scrape_dict = scrape_gds(query_terms = sampleIDs,
+                    api_key = "",
+                    multichannel = multichannel_flag, ### In progress
+                    cells_flag = cells_flag,
+                    metadata_filter = metadata_filter,
+                    DEBUG=1,
+                    out_path = out_path,
+                    keep_files = keep_files_list,
+                    out_types = out_types_list,
+                    run_type = run_type,
+                    local_files_list = local_files_list
+                    )
+    print(f'Scrape successful. File saved to {out_path}')
+    return scrape_dict
+
+
+#######################################################
+
+### SampleIDs parameters
+num_samples = 0
+### Set to False for random sample, else pull `num_samples` samples consecutively starting at a random sample.
+samples_in_order = False
+
+sampleIDs = pd.read_csv('data/GEO_MusmusculusSamples.csv', index_col=[0], names=['sample_ID'], header=0)['sample_ID'].unique()
+
+if samples_in_order == True:
+    rand_num = random.randint(0, len(sampleIDs) - num_samples)
+    sampleIDs = list(sampleIDs[rand_num:rand_num+num_samples])
+else:
+    sampleIDs = list(np.random.choice(sampleIDs, num_samples))
+
+
+### GSM1010177 - KO extract
+### GSM1373723 - KO extract
+### GSM1339550 - KO extract
+### GSM1524918 - KO extract
+#samples_with_age_char = ['GSM1369233', 'GSM1255384', 'GSM1369233', 'GSM1010177', 'GSM1373723', 'GSM1339550', 'GSM1524918', 'GSM1674196']
+### GSM1034523 - alpha in treatment_protocol
+### GSM1010177 - PMC and Nature articles
+### GSM1083104 - PMC and JVI articles
+#inter_sample = ['GSM1034523', 'GSM1010177', 'GSM1083104']
+test = ['GSM982723']
+sampleIDs = sampleIDs + test
+
+organism = 'Mus musculus'
+out_path = 'output/local_files_list_run'
+multichannel_flag = False
+cells_flag = False
+metadata_filter = True
+keep_files_list = ['txt']
+out_types_list = ['json', 'csv']
+run_type='append'
+local_files_list = ['txt']
+
+if __name__ == "__main__":
+
+    main(sampleIDs, organism, out_path, multichannel_flag, cells_flag, metadata_filter, keep_files_list, out_types_list, run_type, local_files_list)
---
+++ src/scrape_gds.py
@@ -0,0 +1,166 @@
+import xml.etree.ElementTree as ET
+import json, os, time, sys, re, requests, glob
+from tqdm import tqdm
+import pandas as pd
+from src.search_samples import get_sample_data
+from src.geo_parser import geo_txt_parse
+from src.ftp_gzxml_parser import xml_parser
+from src.geo_extraction_funcs import *
+from src.processing_data import final_processing_loop
+
+def scrape_gds(query_terms,
+                api_key,
+                DEBUG = 0,
+                multichannel = False,
+                cells_flag = False,
+                metadata_filter = False,
+                out_path = '/output/test',
+                keep_files = [None],
+                run_type = 'append',
+                out_types = ['json', 'csv'],
+                local_files_list = [None]):
+
+    """
+    Function to take in an iterator of sample IDs and output either a .json or .csv file of the sample, series and platform data and the metadata associated with the sample's series.
+    In `src.search_samples.get_sample_data()`, the NCBI API is used to download text files of samples using the GSM ID. This will return the filename of the downloaded data.
+    The list of filenames and associated sampleIDs is fed to `src.geo_parser.geo_txt_parse()` which will extract the sample's sample, series and platform data. This function returns a dictionary with GSM ID keys and data dictionary values.
+    The series FTP links are fed individual to `src.ftp_gzxml_parser.xml_parser()` to gather metadata on samples in each series. This function returns a dictionary of samples from that series.
+    Before the data is returned, a final parsing loop is run that will update the samples with the sample's metadata, run regex checks over certain text blocks, and run the age-extraction function.
+    The data is returned in the format specified by `out_types` at the location specified in `out_path`. If `run_type` is set to `append`, progam will check for a pre-existing file to append to and create one if there isn't an existing option. Setting `run_type` to `new` will overwrite any existing file at `out_path`.
+
+        Args:
+            `query_terms` - Series/list: Terms to feed into the API
+            `api_key` - Str: If you have an API key, call limit goes up to 10/s so you could lower time.sleep(1) to time.sleep(.5) which may be worth it for pulling 10,000+ records at once
+            `DEBUG` - Int: Adds some DEBUG print options. Being phased out. Soon to deprecate.
+            `multichannel` - Boolean: Flag telling the program to skip samples with multichannels found in meta-data.
+            `cells_flag` - Boolean: Flag telling the program to skip samples with 'cells' data found in meta-data.
+            `metadata_filter` - Boolean: Flag telling the program to skip samples without any series metadata
+            `out_path` - Str: Output path, does not include extension as the correct extension will be added. Defaults to /output/test
+            `keep_files` - List: Flags to keep certain files used during the process. Options include 'txt' and 'xml'.
+            `run_type` - Str: Set to `new` to create a new file at `out_path`. Set to `append` to append to an existing file at `out_path`.
+            `out_types` - List: Output file type. Options include 'json' and 'csv'
+            `local_files_list` - List: List of types of files to check for a local version of before querying API and downloading. Currently only works with `txt` but will add `xml` in the future. 
+
+        Returns:
+            `text_file_dict` - Dict: Contains {sampleID : data} key-value pairs for all the requested samples.
+
+            This function will also create output files.
+                - File type is decided by files in `out_type`. By default, create both CSV and JSON.
+                - Files will be named based on `out_path` with the corresponding extension added.
+                - Files have sampleID as key/index and then associated data as values/columns.
+    """
+
+    for file_type in keep_files:
+        if (file_type != 'txt') and (file_type != 'xml') and (file_type is not None):
+            raise ValueError(f'{file_type} is not a valid option for `keep_files`. Please enter either `xml` and/or `csv`.')
+    for file_type in out_types:
+        if (file_type != 'json') and (file_type != 'csv') and (file_type is not None):
+            raise ValueError(f'{file_type} is not a valid option for `out_type`. Please enter either `csv` and/or `json`.')
+    if (run_type != 'new') and (run_type != 'append'):
+        raise ValueError(f'{run_type} is not a valid option for `run_type`. Please enter either `new` or `append`.')
+    ### `parse_list` elements will be lists containing [filename of sample API data, sample ID]
+    ### [GSM4667_19-10-31-1954_fetch.txt, GSM4667]
+    parse_list=[]
+    print('Creating sample .txt files')
+    query_terms = list(set(query_terms))
+    for query_term in tqdm(query_terms, total=len(query_terms)):
+
+        if 'txt' in local_files_list:
+            search_ids_file = glob.glob(f'output/txt/{query_term}*')
+            if len(search_ids_file) == 0:
+                print('didnt have file')
+                search_ids_file = get_sample_data(query=query_term, api_key=api_key)
+            else:
+                search_ids_file = search_ids_file[0]
+            parse_list.append([search_ids_file, query_term])
+
+        else:
+            search_ids_file = get_sample_data(query=query_term, api_key=api_key)
+            parse_list.append([search_ids_file, query_term])
+        time.sleep(.5)
+
+    """
+    `txt_file_dict` elements are lists containing the following:
+        Include description and cells so later you can filter on them without an Error
+        txt_file_dict[sample_id] = {'sample_id' : query_name,
+                                'series' : series,
+                                'series_accession' : series_accession,
+                                'series_ftp' : series_ftp,
+                                'platform' : platform,
+                                'platform_accession' : platform_accession,
+                                'platform_ftp' : platform_ftp,
+                                'sample' : sample,
+                                'contents' : contents,
+                                'description' : '',
+                                'cells' : ''}
+
+    """
+    print('Parsing the .txt files.')
+    text_file_dict = geo_txt_parse(parse_list, keep_files = keep_files)
+
+    ### Lists for later
+    samples_series_accession_numbers = [i['series_accession'] for i in text_file_dict.values()]
+    samples_series_ftp_links = [i['series_ftp'] for i in text_file_dict.values()]
+
+    """
+    Prepare a slot in the metadata dictionary for all the requested samples.
+        - Add blank sample_age to each one to use it for the geoAgeExtract()
+    Also created a series -> PMID data dict using all the requested samples series' number
+        - Also add blank PMID, Series Summary and Design
+    """
+    samples_metadata_dict = {key:{'sample_age' : ''} for key in query_terms}
+    series_pmid_dict = {key: {'pmid':'', 'series_summary': '', 'series_design':''} for key in samples_series_accession_numbers}
+
+    print('Downloading and collecting meta-data')
+    for url in tqdm(list(set(samples_series_ftp_links)), total=len(list(set(samples_series_ftp_links)))):
+        if url == None:
+            ### Empty dict if broken URL
+            series_meta_dict = dict()
+            xml_pmid_list = []
+        else:
+            """
+            `xml_parser` will take in a Series FTP Download URL and return,
+                -   `xml_pmid_list` is a dictionary of series -> PMID data mappings
+                -   `series_meta_dict` is a dictionary of the series meta data
+            """
+            xml_pmid_list, series_meta_dict = xml_parser(url, sample_list=query_terms, multichannel = multichannel, keep_files = keep_files)
+        ### Update relevant dictionaries
+        series_pmid_dict.update(xml_pmid_list)
+        samples_metadata_dict.update(series_meta_dict)
+
+    ### Do final processing on samples
+    ### Start by adding metadata then adding derived fields
+    print('Final parsing loop')
+    text_file_dict = final_processing_loop(text_file_dict, samples_metadata_dict, series_pmid_dict, multichannel, metadata_filter, cells_flag)
+
+    ### Export data
+    if run_type == 'new':
+        if 'json' in out_types:
+            with open(f'{out_path}.json', 'w') as fout:
+                json.dump(text_file_dict, fout, indent = 4)
+        if 'csv' in out_types:
+            pd.DataFrame.from_dict(text_file_dict, orient='index').to_csv(f'{out_path}.csv')
+
+    elif run_type == 'append':
+        if 'json' in out_types:
+            try:
+                with open(f'{out_path}.json', 'r') as f:
+                    pre_existing_data = json.load(f)
+            except:
+                print('No pre-existing data.')
+                pre_existing_data = dict()
+            text_file_dict = {k : v for (k,v) in text_file_dict.items() if k not in pre_existing_data.keys()}
+            pre_existing_data.update(text_file_dict)
+            with open(f'{out_path}.json', 'w') as fout:
+                json.dump(pre_existing_data, fout, indent = 4)
+        if 'csv' in out_types:
+            try:
+                pre_existing_data = pd.read_csv(f'{out_path}.csv', header=0, index_col=0).to_dict(orient='index')
+            except:
+                print('No pre-existing data.')
+                pre_existing_data = dict()
+            text_file_dict = {k : v for (k,v) in text_file_dict.items() if k not in pre_existing_data.keys()}
+            pre_existing_data.update(text_file_dict)
+
+            pd.DataFrame.from_dict(pre_existing_data, orient='index').to_csv(f'{out_path}.csv')
+    return text_file_dict
